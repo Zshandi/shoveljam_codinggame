be63978654156ade967fcad7ea94614bca2df251
Lachlannm <49292111+Lachlannm@users.noreply.github.com>
2025-07-16 23:39:53 -0700

    add crt filter and floppy disk goal

--- code_unedit.gd
+++ code_unedit.gd
@@ -5,7 +5,11 @@ var original_text
 func _ready() -> void:
 	original_text = text
 	text_changed.connect(self._on_text_changed)
+	text_set.connect(self._on_text_set)
 	context_menu_enabled = false
 
 func _on_text_changed() -> void:
 	text = original_text
+
+func _on_text_set():
+	original_text = text
