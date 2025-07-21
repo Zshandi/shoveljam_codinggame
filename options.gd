5e058f0b86ad35fe404d2d29f70dfaf7a78dfdf8
Zshandi Krahn <ZshandiK@gmail.com>
2025-07-20 17:00:29 -0700

    Add code execution speed setting

--- options.gd
+++ options.gd
@@ -31,12 +31,22 @@ var is_full_screen:bool:
 
 var typing_sound_enabled:bool = true
 
+const min_code_exec_time_ms_default := 650
+var min_code_exec_time_ms:int = min_code_exec_time_ms_default
+
+var code_execution_speed:int:
+	get:
+		return clampi(min_code_exec_time_ms / min_code_exec_time_ms_default, 0, 2)
+	set(value):
+		min_code_exec_time_ms = min_code_exec_time_ms_default * clamp(value, 0, 2)
+
 func save_settings() -> void:
 	var save_dict := {}
 	save_dict["music_volume"] = music_volume
 	save_dict["sound_volume"] = sound_volume
 	save_dict["is_full_screen"] = is_full_screen
 	save_dict["typing_sound_enabled"] = typing_sound_enabled
+	save_dict["code_execution_speed"] = code_execution_speed
 	
 	var save_file := FileAccess.open("user://settings", FileAccess.WRITE)
 	save_file.store_var(save_dict)
@@ -55,6 +65,8 @@ func load_settings() -> void:
 	sound_volume = save_dict["sound_volume"]
 	is_full_screen = save_dict["is_full_screen"]
 	typing_sound_enabled = save_dict["typing_sound_enabled"]
+	if "code_execution_speed" in save_dict:
+		code_execution_speed = save_dict["code_execution_speed"]
 
 func _ready() -> void:
 	load_settings()
--- options_menu.gd
+++ options_menu.gd
@@ -4,6 +4,7 @@ func _ready() -> void:
 	Options.load_settings()
 	%MusicVolumeSlider.set_value_no_signal(Options.music_volume)
 	%SoundVolumeSlider.set_value_no_signal(Options.sound_volume)
+	%ExecutionSpeedSlider.set_value_no_signal(Options.code_execution_speed)
 	%TypingSoundButton.set_pressed_no_signal(Options.typing_sound_enabled)
 	%FullScreenButton.set_pressed_no_signal(Options.is_full_screen)
 
@@ -30,3 +31,8 @@ func _on_music_volume_slider_value_changed(value: float) -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_execution_speed_slider_value_changed(value: float) -> void:
+	Options.code_execution_speed = int(value)
+	Options.save_settings()
